3348bc5e29cfea81bf5cdd942ca6818dc4a7c78d
Ryan1729 <Ryan1729@gmail.com>
2016-10-23 03:29:13 -0600

    get sizing set up and draw circle as placeholder
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- View.elm
+++ View.elm
@@ -5,6 +5,9 @@ import Html exposing (Html, text, div)
 import Html.Attributes exposing (style)
 import Msg exposing (Msg(..))
 import Material.Button as Button
+import Svg exposing (Svg, svg, circle, Attribute)
+import Svg.Attributes exposing (..)
+import Svg.Events exposing (onClick)
 
 
 red : String
@@ -27,10 +30,79 @@ yellow =
     "#EFEFAB"
 
 
+gameWidth : Int
+gameWidth =
+    600
+
+
+gameHeight : Int
+gameHeight =
+    600
+
+
+widthString =
+    toString gameWidth
+
+
+heightString =
+    toString gameHeight
+
+
+sectionWidth : Int
+sectionWidth =
+    gameWidth // 5
+
+
+sectionHeight : Int
+sectionHeight =
+    gameHeight // 5
+
+
+sectionWidthString : String
+sectionWidthString =
+    toString sectionWidth
+
+
+sectionHeightString : String
+sectionHeightString =
+    toString sectionHeight
+
+
+halfSectionWidth : Int
+halfSectionWidth =
+    sectionWidth // 2
+
+
+halfSectionHeight : Int
+halfSectionHeight =
+    sectionHeight // 2
+
+
+halfSectionWidthString : String
+halfSectionWidthString =
+    toString halfSectionWidth
+
+
+halfSectionHeightString : String
+halfSectionHeightString =
+    toString halfSectionHeight
+
+
+viewBoxString =
+    "-"
+        ++ halfSectionWidthString
+        ++ " -"
+        ++ halfSectionHeightString
+        ++ " "
+        ++ sectionWidthString
+        ++ " "
+        ++ sectionHeightString
+
+
 view : Model -> Html Msg
 view model =
     div
-        [ style
+        [ Html.Attributes.style
             [ ( "display", "flex" )
             , ( "flex-direction", "column" )
             , ( "align-items", "center" )
@@ -48,7 +120,7 @@ view model =
             |> toString
             |> text
         , rackDescription blue model.blueRack
-            |> List.map sectionView
+            |> List.map (sectionView model)
             |> div []
         ]
 
@@ -75,11 +147,14 @@ boardSectionDescription section =
         (descriptionFromBoardState section.small)
 
 
-sectionView : RingsDescription -> Html Msg
-sectionView (RingsDescription largeDescription mediumDescription smallDescription) =
-    ( largeDescription, mediumDescription, smallDescription )
-        |> toString
-        |> text
+sectionView : Model -> RingsDescription -> Html Msg
+sectionView model (RingsDescription largeDescription mediumDescription smallDescription) =
+    div []
+        [ svg [ width sectionWidthString, height sectionHeightString, viewBox viewBoxString ] [ circle [ r "30", fill "#7F9Fff" ] [] ]
+        , ( mediumDescription, smallDescription )
+            |> toString
+            |> text
+        ]
 
 
 type RingDescription
@@ -89,10 +164,6 @@ type RingDescription
 
 type RingsDescription
     = RingsDescription RingDescription RingDescription RingDescription
-
-
-
--- (,,)
 
 
 descriptionFromRack : String -> Bool -> RingDescription
