898fab8e9f27c32650a7143df4448a94cc94b4fa
Ryan1729 <Ryan1729@gmail.com>
2016-10-22 21:44:25 -0600

    add dummy new game button and model board
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- Model.elm
+++ Model.elm
@@ -4,8 +4,58 @@ import Material
 
 
 type alias Model =
-    { mdl : Material.Model }
+    { mdl : Material.Model
+    , board : Board
+    }
 
 
 defaultState =
-    { mdl = Material.model }
+    { mdl = Material.model
+    , board = emptyBoard
+    }
+
+
+emptyBoard : Board
+emptyBoard =
+    Board emptyBoardSection emptyBoardSection emptyBoardSection
+
+
+type alias Board =
+    { large : BoardSection
+    , medium : BoardSection
+    , small : BoardSection
+    }
+
+
+emptyBoardSection : BoardSection
+emptyBoardSection =
+    BoardSection Empty
+        Empty
+        Empty
+        Empty
+        Empty
+        Empty
+        Empty
+        Empty
+        Empty
+
+
+type alias BoardSection =
+    { topLeft : SectionState
+    , topMiddle : SectionState
+    , topRight : SectionState
+    , leftMiddle : SectionState
+    , middle : SectionState
+    , rightMiddle : SectionState
+    , bottomLeft : SectionState
+    , bottomMiddle : SectionState
+    , bottomRight : SectionState
+    }
+
+
+type SectionState
+    = Red
+    | Blue
+    | Green
+    | Yellow
+    | Empty
--- View.elm
+++ View.elm
@@ -1,23 +1,30 @@
 module View exposing (view)
 
 import Model exposing (Model)
-import Html exposing (Html, text)
+import Html exposing (Html, text, div)
+import Html.Attributes exposing (style)
 import Msg exposing (Msg(..))
 import Material.Button as Button
 
 
 view : Model -> Html Msg
 view model =
-    Html.div []
-        [ model
-            |> toString
-            |> text
-        , Button.render Mdl
+    div
+        [ style
+            [ ( "display", "flex" )
+            , ( "flex-direction", "column" )
+            , ( "align-items", "center" )
+            ]
+        ]
+        [ Button.render Mdl
             [ 0 ]
             model.mdl
             [ Button.raised
             , Button.ripple
             , Button.onClick NoOp
             ]
-            [ text "test Button" ]
+            [ text "New Game" ]
+        , model.board
+            |> toString
+            |> text
         ]
